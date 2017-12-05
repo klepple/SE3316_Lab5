c8e3bf4623b384bd635525a7812734e98e629c6c
Kahlia Lepple <klepple@uwo.ca>
2017-12-05 00:08:15 +0000

    Edit UI collection creation

--- server/models/collection-model.js
+++ server/models/collection-model.js
@@ -26,6 +26,7 @@ module.exports.addCollection = function(newCollection, callback){
     newCollection.save(callback);
 }
 
+
 /*module.exports.addImageToCollection = function(collection, callback){
     this.getCollectionByName(collection.name, callback);
     collection.save(callback);
