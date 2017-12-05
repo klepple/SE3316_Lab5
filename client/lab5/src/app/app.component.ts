4bec54a65c179d7135aca64a5a206099ebda21c9
Kahlia Lepple <klepple@uwo.ca>
2017-12-05 00:15:52 +0000

    Clean up app component

--- client/lab5/src/app/app.component.ts
+++ client/lab5/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 
-// Import the DataService
-import { DataService } from './data.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,12 +7,8 @@ import { DataService } from './data.service';
 })
 export class AppComponent {
   title = 'Reach For the Stars';
-  
-   // Define a users property to hold our user data
-  users: Array<any>;
 
-  // Create an instance of the DataService through dependency injection
-  constructor(private _dataService: DataService) {
+  constructor() {
   }
 
 }
